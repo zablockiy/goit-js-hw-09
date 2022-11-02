943fb9571b0f0ed262e4d5353393e0d2d8cd34da
zablockiy <105744371+zablockiy@users.noreply.github.com>
2022-11-02 13:12:34 +0000

    Deploying to gh-pages from @ zablockiy/goit-js-hw-09@ab998f2d4e0bce08ee21879557eedfe91e6ada4f 🚀

--- 03-promises.020ef84b.js
+++
@@ -1,2 +0,0 @@
-(void Math.random()).then((({position:o,delay:e})=>{console.log(`✅ Fulfilled promise ${o} in ${e}ms`)})).catch((({position:o,delay:e})=>{console.log(`❌ Rejected promise ${o} in ${e}ms`)}));
-//# sourceMappingURL=03-promises.020ef84b.js.map
--- 03-promises.a16f016c.js
+++
@@ -1,2 +0,0 @@
-(void Math.random()).then((function(o){var n=o.position,c=o.delay;console.log("✅ Fulfilled promise ".concat(n," in ").concat(c,"ms"))})).catch((function(o){var n=o.position,c=o.delay;console.log("❌ Rejected promise ".concat(n," in ").concat(c,"ms"))}));
-//# sourceMappingURL=03-promises.a16f016c.js.map
